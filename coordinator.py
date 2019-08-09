e27474323d7b7561a1dc8c4175166cc868bd65da
unknown <cesar.garza.826@gmail.com>
2019-08-09 18:40:13 -0500

    Geocoding thanks to Texas A&M Geoservices

--- coordinator.py
+++ coordinator.py
@@ -1,19 +1,27 @@
-import geocoder
-import sqlite3, os
+import sqlite3, os, csv
 
-import keys
+#Geocoding courtesy of Texas A&M Geoservices
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 cxn = sqlite3.connect(__location__+'\zillow.db')
 c = cxn.cursor()
-c.execute("Select * from listings")
 
-curr_row = c.fetchone()
-address = curr_row[1]
-city = curr_row[-2]
-zipcode = str(curr_row[-1])
+sql = """update listings
+set lat = {insert_lat}, lon = {insert_lon} where address="{insert_address}";"""
 
-print(geocoder.google(' '.join([address, city, zipcode])))
+#Add latitude and longitude to corresponding addresses
+with open('zillow_geocoded.csv') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(csv_reader)
+    for row in csv_reader:
+        address = row[0]
+        latitude = row[-3]
+        longitude = row[-2]
+        sql1 = sql.format(insert_lat = latitude, insert_lon = longitude, insert_address = address)
+        c.execute(sql1)
+        cxn.commit()
+    
+cxn.commit()
 
 cxn.close()
